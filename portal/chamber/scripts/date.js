f124efd32dfa10f82ba14bd6c41d3a040a5ec789
venus003 <nlstolp@hotmail.com>
2023-02-04 19:15:23 -0600

    site updates

--- portal/chamber/scripts/date.js
+++ portal/chamber/scripts/date.js
@@ -3,3 +3,7 @@ const today = new Date();
 const here = new Intl.DateTimeFormat("en-UK", {dateStyle: "full"}).format(today);
 
 document.querySelector("#now").innerHTML = here;
+
+if (today.getDay() == 2) {
+    document.querySelector("#tuesdays").innerHTML = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
+}
